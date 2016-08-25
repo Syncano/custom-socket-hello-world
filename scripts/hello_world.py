8b6b87484806d4ffc3b381f61c538265e524f42a
Sebastian Opałczyński <opalczynski@gmail.com>
2016-08-25 16:46:43 +0200

    [HELLOWORLD] provide basic hello world example;

---
+++ scripts/hello_world.py
@@ -0,0 +1,15 @@
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello world!</title>
+</head>
+
+<body>
+Hello World!
+</body>
+
+</html>
+"""
+
+set_response(HttpResponse(status_code=200, content=content, content_type='text/html'))
